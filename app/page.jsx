1a193ef84d740506c000ad16c0e67be52af11aaa
anshumandev2002 <anshumandev2002@gmail.com>
2025-01-09 08:17:26 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button, ButtonGroup } from "@nextui-org/button";
+const page = () => {
+  return (
+    <Button isLoading color="primary">
+      Loading
+    </Button>
+  );
+};
+
+export default page;
